99a078a906a3f6b7873886486b0e6317a7a4a25c
Nightfeller <family2earls@gmail.com>
2024-01-09 18:40:42 -0600

    slightly modified the starter code and added some things in the webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+
+  } catch (err) {
+  console.log(err);
+  console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+
+  } catch (err) {
+  console.log(err);
+  console.error('getDb not implemented');
+  }
+};
 
 initdb();
